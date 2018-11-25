fce19975d7a0ccde2d1a227b51bff94ccd148ac2
jmrodas <rodasdeveloper@gmail.com>
2018-11-25 10:32:16 +0100

    Comienzo single page app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { BodyComponent } from './components/body/body.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { BodyComponent } from './components/shared/body/body.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
